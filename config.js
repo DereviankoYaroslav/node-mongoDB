18dd8b141de76c16e91a351c2df89fcdd5650375
DereviankoYaroslav <yarik0009258@gmail.com>
2022-10-06 19:39:51 +0300

    OAuth2.0

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1750877921972080',
+        clientSecret: '762bcea0723c2802105b9e5f105ad9aa'
+    }
 }
